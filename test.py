3408b5c176f260bf8223e8db8fb30328a204dd69
Mairene Sumalague <mayewoon1924@gmail.com>
2022-11-01 16:06:14 +0800

    Add files via upload
    update

---
+++ test.py
@@ -0,0 +1,54 @@
+import pickle
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import pickle as p
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+#Loading up the Regression model we created
+model = pickle.load(open('FinalDCT.pkl','rb'))
+
+#dataset
+data_sample = pd.read_csv("importance.csv")
+
+#Text
+st.title('URL Classification Prediction Model')
+st.header(
+          'This project aims to provide a comprehensive survey and a structural understanding of Malicious URL Detection techniques using machine learning. ')
+
+st.text("Kindly fill out all the input fields in order to see the results")
+
+st.dataframe(data_sample)
+
+#path_token_count = st.number_input("Path Token Count")
+average_domain_token = st.number_input("Average Domain Token")
+entropy_url = st.number_input("Entropy URL")
+charcompvowels = st.number_input("Charcomp Vowels")
+charcompace = st.number_input("Charcomp Ace")
+path_url_ratio = st.number_input("Path Url Ratio")
+domain_url_ratio = st.number_input("Domain URL Ratio")
+symbol_count_url = st.number_input("Symbol Count URL")
+
+
+# Training the data using decision tree
+dtc_clf=DecisionTreeClassifier()
+predict_val = model.predict([['path_token_count', 'average_domain_token', 'entropy_url', 'charcompvowels', 'charcompace', 'path_url_ratio', 'domain_url_ratio', 'symbol_count_url']])
+predict_val = float(predict_val)
+
+if predict_val == 1:
+    st.info("URL Type: Benign")
+
+elif predict_val == 0:
+    st.info("URL Type: Defacement")
+
+elif predict_val == 2:
+    st.info("URL Type: Malware")
+
+elif predict_val == 3:
+    st.info("URL Type: Phishing")
+
+else:
+    st.info("URL Type: Spam")
